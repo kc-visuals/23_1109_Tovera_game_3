8222aa221a51096646a57afb14ebc1ce3a3c816d
Toby L <ultimatemoomshroom@gmail.com>
2023-11-16 14:17:19 -0500

    added demon and demon AI

--- Assets/Scripts/DemonScript.cs
+++ Assets/Scripts/DemonScript.cs
@@ -1,18 +1,133 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class DemonScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    float jumpAttackTime;
+
+    public bool isAttacking;
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    Rigidbody rb;
+
+    //patrolling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    public void Awake()
     {
-        
+        //player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        //Debug.Log("test");
+
+        if (!isAttacking)
+        {
+            if (!playerInSightRange && !playerInAttackRange) Patrolling();
+            if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+            if (playerInAttackRange && playerInSightRange) AttackPlayer();
+        }
         
+    }
+
+    void Patrolling()
+    {
+        //Debug.Log("patrol");
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //Walkpoint Reached
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+    }
+    void ChasePlayer()
+    {
+        //Debug.Log("chase");
+        agent.SetDestination(player.position);
+    }
+    void AttackPlayer()
+    {
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z));
+
+        if(!alreadyAttacked)
+        {
+            //AttackCode
+            Debug.Log("ATTACK");
+            agent.enabled = false;
+            isAttacking = true;
+            StartCoroutine(JumpAttackCoroutine());
+            rb.isKinematic = false;
+            rb.velocity = Vector3.zero;
+            rb.freezeRotation = true;
+            rb.AddForce(transform.forward * 700 + transform.up * 200);
+            
+
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+    void SearchWalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = true;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+
+    IEnumerator JumpAttackCoroutine()
+    {
+        yield return new WaitForSeconds(jumpAttackTime);
+        isAttacking = false;
+        agent.enabled = true;
+        rb.freezeRotation = false;
     }
 }
