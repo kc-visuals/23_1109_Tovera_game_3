698349b3e05acb1527b910a483175f4c1b01c73a
Toby L <ultimatemoomshroom@gmail.com>
2023-11-16 01:58:56 -0500

    added reload and shot delay

--- Assets/Scripts/Player Scripts/ShootingScript.cs
+++ Assets/Scripts/Player Scripts/ShootingScript.cs
@@ -13,8 +13,51 @@ public class ShootingScript : MonoBehaviour
     [SerializeField]
     GameObject mainCamera;
 
+    [SerializeField]
+    float reloadTime;
+
+    public int ammo = 6;
+    bool readyToFire;
+
+    [SerializeField]
+    float shotTimeFloat;
+
+    WaitForSeconds shotTime;
+
+    private void Start()
+    {
+        shotTime = new WaitForSeconds(shotTimeFloat);
+        readyToFire = true;
+    }
     void OnFire()
     {
-        Instantiate(bullet, gun.transform.position, mainCamera.transform.rotation);
+        if (readyToFire)
+        {
+            if (ammo > 0)
+            {
+                Instantiate(bullet, gun.transform.position, mainCamera.transform.rotation);
+                ammo--;
+                readyToFire = false;
+                StartCoroutine(ShotDelay());
+            }
+        }
+    }
+
+    void OnReload()
+    {
+        StartCoroutine(ReloadCoroutine());
+        //play particle effect for reload spell
+    }
+
+    IEnumerator ReloadCoroutine()
+    {
+        yield return new WaitForSeconds(reloadTime);
+        ammo = 6;
+    }
+
+    IEnumerator ShotDelay()
+    {
+        yield return shotTime;
+        readyToFire = true;
     }
 }
