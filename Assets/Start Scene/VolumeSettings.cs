19513bff37bf2b6e918e0f547c83b18140a287c2
kc-visuals <karlatovera@gmail.com>
2023-12-13 21:50:02 -0500

    Fixed audio script and added audio

--- Assets/Start Scene/VolumeSettings.cs
+++ Assets/Start Scene/VolumeSettings.cs
@@ -6,12 +6,40 @@ using UnityEngine.UI;
 
 public class VolumeSettings : MonoBehaviour
 {
+    private static VolumeSettings instance;
     [SerializeField] private AudioMixer myMixer;
     [SerializeField] private Slider audioSlider;
-
-    public void SetGameVolume()
+    [SerializeField] private AudioSource[] audioSources;
+    void Awake()
     {
-        float volume = audioSlider.value;
-        myMixer.SetFloat("GameAudio", volume);
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start()
+    {
+        audioSlider.value = GetVolume();
+        audioSlider.onValueChanged.AddListener(SetVolume);
+    }
+    public void SetVolume(float volume)
+    {
+        foreach (var audioSource in audioSources)
+        {
+            // Adjust the volume of each AudioSource individually
+            audioSource.volume = volume;
+        }
+        myMixer.SetFloat("GameVolume", Mathf.Log10(volume) * 20);
+    }
+
+    public float GetVolume()
+    {
+       
+        return audioSources.Length > 0 ? audioSources[0].volume : 1.0f;
     }
 }
